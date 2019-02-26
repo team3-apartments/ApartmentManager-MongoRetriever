a6ffbe6da45cba2ae41d30fd07df517828d12d18
jgurney1 <41511563+jgurney1@users.noreply.github.com>
2019-02-26 19:09:24 +0100

    Update App.java

--- src/main/java/com/qa/apartmentManager/App.java
+++ src/main/java/com/qa/apartmentManager/App.java
@@ -2,9 +2,6 @@ package com.qa.apartmentManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class App {
